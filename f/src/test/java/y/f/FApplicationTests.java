d1c73d3f2887402eebe1162709940c554dc9a4b3
junyeongbluefin <junyeong@bluefinenterprises.com>
2019-11-12 21:25:24 -0500

    F

---
+++ f/src/test/java/y/f/FApplicationTests.java
@@ -0,0 +1,17 @@
+package y.f;
+
+import org.springframework.cloud.function.context.test.FunctionalSpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(SpringRunner.class)
+@FunctionalSpringBootTest
+public class FApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
