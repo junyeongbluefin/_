7e96c5352e106a1d99139b55d5167559ea67a0f7
junyeongbluefin <junyeong@bluefinenterprises.com>
2019-11-18 19:58:11 -0500

    U

--- cl/src/main/java/y/cl/HelloController.java
+++ cl/src/main/java/y/cl/HelloController.java
@@ -2,18 +2,12 @@ package y.cl;
 
 
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
 public class HelloController {
-
-//    @Autowired
-//    @Value("${info.foo}")
-//    private String foo;
 
     private final InfoProperties infoProperties;
 
